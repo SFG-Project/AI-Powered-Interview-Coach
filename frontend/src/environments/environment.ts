1b7a401bf3c8b28a80c39bd2c18c7fc35c364fd5
MolokoV2 <molokomodubi@outlook.com>
2026-05-14 16:54:17 +0200

    updated QA env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  appName: "AI-Powered Adaptive Interview Coach",
-  apiBaseUrl: "https://ai-powered-interview-coach-eurt.onrender.com",
+  appName: "AI-Powered Adaptive Interview Coach QA",
+  apiBaseUrl: "https://ai-powered-interview-coach-qa.onrender.com",
 };
